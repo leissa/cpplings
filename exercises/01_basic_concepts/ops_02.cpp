4e46ac3b31c246e41f4526c90ad47fd08f613224
HarithF <al-harith.farhad@uni-goettingen.de>
2026-04-09 14:56:44 +0200

    update exercise

--- exercises/01_basic_concepts/ops_02.cpp
+++ exercises/01_basic_concepts/ops_02.cpp
@@ -1,6 +1,6 @@
 #include <doctest/doctest.h>
 
 // TODO: add parentheses to make the expression evaluate to 10.
-int result() { return 2 + 3 * 4 - 4; }
+int result() { return 2 + 3 * 3 - 5; }
 
 TEST_CASE("precedence") { CHECK(result() == 10); }
